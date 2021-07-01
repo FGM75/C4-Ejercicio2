70100c077d0745e0602daaafc2ce244017b57f58
FGM75 <fgonzalezm75@gmail.com>
2021-07-01 18:22:11 +0200

    esto es todo amigooos.

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const os = require("os");
+const path = require("path");
+const fs = require("fs");
+console.log("Gatitos hackeando tu PC...");
 setTimeout((sistemaOp) => {
   if (process.platform === "win32") {
     console.log("Tú no molas mucho");
@@ -9,10 +12,29 @@ setTimeout((sistemaOp) => {
   } else if (process.platform === "linux") {
     console.log("Tú molas");
   }
-});
 
-const mbLibre = os.freemem() / 1048576;
+  const mbLibre = os.freemem() / 1048576;
 
-console.log("Cuidado, te quedan " + mbLibre + "Mb de RAM libre");
+  console.log("Cuidado, te quedan " + mbLibre + " Mb libres");
 
-console.log("La version de tu sistema es: " + os.version());
+  console.log("La version de tu sistema es: " + os.version());
+
+  console.log(
+    "Tu usuario del sistema operativo es " +
+      path.basename(os.homedir()) +
+      " y tu carpeta es " +
+      os.homedir()
+  );
+
+  fs.readdir(os.homedir(), (err, archivos) => {
+    if (err) {
+      console.log("Algo ha petado");
+      return;
+    }
+    for (const archivo of archivos) {
+      fs.stat(path.join(os.homedir(), archivo), (err, info) => {
+        console.log(archivo + "y ocupa " + info.size + " Kb");
+      });
+    }
+  });
+}, 1000);
